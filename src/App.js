3fa386f22cad451cab46ea3cd6925a4cd6ed0eac
davidbeleznay <dbeleznay09@gmail.com>
2025-12-08 20:14:15 -0800

    Remove culvert CSS imports from App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import AppRouter from './navigation/AppRouter';
 import './styles/index.css';
-import './styles/culvert-form.css';
 import './styles/home.css';
-import './styles/progress-steps.css';
-import './styles/info-components.css';
-import './styles/culvert-results.css';
 import './styles/RoadRiskForm.css';
 import './styles/form-elements.css';
 import './styles/form-sections.css';
