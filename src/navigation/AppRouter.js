0593969a4eac86dc87a3b0b092827ba29a0ecc5e
davidbeleznay <dbeleznay09@gmail.com>
2025-12-08 20:04:14 -0800

    Remove culvert routes from AppRouter - road risk only

--- src/navigation/AppRouter.js
+++ src/navigation/AppRouter.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomeScreen from '../screens/HomeScreen';
 import RoadRiskForm from '../pages/RoadRiskForm';
-import CulvertSizingForm from '../pages/CulvertSizingForm';
 import HistoryPage from '../pages/HistoryPage';
 import Dashboard from '../pages/Dashboard';
 
@@ -14,8 +13,6 @@ const AppRouter = () => {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/road-risk" element={<RoadRiskForm />} />
         <Route path="/road-risk/edit/:id" element={<RoadRiskForm />} />
-        <Route path="/culvert" element={<CulvertSizingForm />} />
-        <Route path="/culvert/edit/:id" element={<CulvertSizingForm />} />
         <Route path="/history" element={<HistoryPage />} />
       </Routes>
     </Router>
