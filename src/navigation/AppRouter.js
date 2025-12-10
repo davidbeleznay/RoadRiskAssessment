cc954052052ef8a50f879ec4981671221ad7bac4
davidbeleznay <dbeleznay09@gmail.com>
2025-12-09 17:50:04 -0800

    Add route for LMH risk assessment form

--- src/navigation/AppRouter.js
+++ src/navigation/AppRouter.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomeScreen from '../screens/HomeScreen';
 import RoadRiskForm from '../pages/RoadRiskForm';
+import LMHRiskForm from '../pages/LMHRiskForm';
 import HistoryPage from '../pages/HistoryPage';
 import Dashboard from '../pages/Dashboard';
 
@@ -12,6 +13,7 @@ const AppRouter = () => {
         <Route path="/" element={<HomeScreen />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/road-risk" element={<RoadRiskForm />} />
+        <Route path="/lmh-risk" element={<LMHRiskForm />} />
         <Route path="/road-risk/edit/:id" element={<RoadRiskForm />} />
         <Route path="/history" element={<HistoryPage />} />
       </Routes>
